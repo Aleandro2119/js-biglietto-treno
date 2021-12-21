0ba1172b7b9a79788fb31ed017df1f2df8c27315
Aleandro2119 <aleandro.basilico@gmail.com>
2021-12-21 15:50:16 +0100

    Add analisi

--- js/script.js
+++ js/script.js
@@ -10,3 +10,13 @@ va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 */
+
+/* 
+1- Chiedere all'utente quanti chilometri vuole percorrere
+2- Calcolare il prezzo del biglietto
+2b- Costo 0.21€ al Km
+  - Sconto del 20% per i minorenni
+  - Sconto del 40% per over 65
+3- Chiedere l'età per applicare sconto
+4- Prezzo finale (con massimo due decimali)
+*/
